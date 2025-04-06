4924fd2eb978ed8ce3e27eb6256dffbb0286eeaf
bizlal <bilal@limelight.io>
2025-04-06 13:11:51 -0400

    Add apiSecret to Audius SDK initialization and uncomment upload track API handler

--- components/ConnectAudius/ConnectSpotify/AudiusSDK.jsx
+++ components/ConnectAudius/ConnectSpotify/AudiusSDK.jsx
@@ -3,7 +3,11 @@ import { sdk } from '@audius/sdk';
 let audiusSdk = null;
 
 const initAudiusSdk = () => {
-  audiusSdk = sdk({ appName: 'limelight', apiKey: process.env.AUDIUS_KEY });
+  audiusSdk = sdk({
+    appName: 'limelight',
+    apiKey: process.env.AUDIUS_KEY,
+    apiSecret: process.env.AUDIUS_SECRET,
+  });
 };
 
 initAudiusSdk();
